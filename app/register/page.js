72799ef36bf37e3ed8c351e0c89c5f0976eed42e
Laura Parra <lparra@beateam.es>
2025-06-12 20:32:18 +0200

    arreglos

--- app/register/page.js
+++ app/register/page.js
@@ -1,26 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-// import { useState } from 'react'
-// import { login } from '@/services/authService'
+import Head from "next/head";
+import Image from "next/image";
 
-const LoginPage = () => {
-//   const [email, setEmail] = useState('')
-
-// const handleSubmit = async (e) => {
-//   e.preventDefault()
-//   const response = await login(email)
-// }
-
+const RegisterPage = () => {
   return (
     <>
       <Head>
         <title>Login</title>
       </Head>
 
-      <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/fondo-amigo-invisible.png')" }}>
+      <div
+        className="relative min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url('/images/fondo-amigo-invisible.png')" }}
+      >
         <div className="absolute inset-0 bg-white/70 backdrop-blur-sm">
           <div className="relative z-10 flex flex-col min-h-screen items-center justify-between p-6 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-1 items-center justify-center w-3xl xs:w-1/2">
+            <main className="flex flex-1 items-center justify-center w-3xl">
               <div className="flex flex-col items-center justify-center w-full gap-6 bg-white p-6 rounded-2xl shadow-lg">
                 <Image
                   className="dark:invert"
@@ -31,18 +25,29 @@ const LoginPage = () => {
                   priority
                 />
                 <form className="flex flex-col gap-4 w-full">
-                  <input
-                    placeholder="Correo"
-                    className="border p-3 rounded-2xl border-gray-300 w-full"
-                    //onSubmit={handleSubmit}
-                    //value={email}
-                    //onChange={(e) => setEmail(e.target.value)}
-                  />
+                  <div className="flex flex-wrap gap-4">
+                    <input
+                      placeholder="Correo 1"
+                      className="border p-3 rounded-2xl border-gray-300 w-[48%]"
+                    />
+                    <input
+                      placeholder="Correo 2"
+                      className="border p-3 rounded-2xl border-gray-300 w-[48%]"
+                    />
+                    <input
+                      placeholder="Correo 3"
+                      className="border p-3 rounded-2xl border-gray-300 w-[48%]"
+                    />
+                    <input
+                      placeholder="Correo 4"
+                      className="border p-3 rounded-2xl border-gray-300 w-[48%]"
+                    />
+                  </div>
                   <button
                     type="submit"
                     className="bg-amber-700 text-white p-3 rounded-2xl w-full font-semibold hover:bg-amber-600 transition"
                   >
-                    Entrar
+                    Registrar
                   </button>
                 </form>
               </div>
@@ -64,7 +69,12 @@ const LoginPage = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Image src="/window.svg" alt="Window icon" width={16} height={16} />
+                <Image
+                  src="/window.svg"
+                  alt="Window icon"
+                  width={16}
+                  height={16}
+                />
                 Examples
               </a>
               <a
@@ -73,7 +83,12 @@ const LoginPage = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Image src="/globe.svg" alt="Globe icon" width={16} height={16} />
+                <Image
+                  src="/globe.svg"
+                  alt="Globe icon"
+                  width={16}
+                  height={16}
+                />
                 Go to nextjs.org →
               </a>
             </footer>
@@ -81,7 +96,7 @@ const LoginPage = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default RegisterPage;
